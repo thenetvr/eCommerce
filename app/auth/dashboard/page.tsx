dab3ccdb28e16531ca6ae8995ec5b6008dacfc09
Noah Fajarda <noahfajarda@gmail.com>
2023-11-07 15:57:39 -0800

    added reroutes to *all* pages IF they do or don't have proper credentials

--- app/auth/dashboard/page.tsx
+++ app/auth/dashboard/page.tsx
@@ -1,7 +1,17 @@
 import UserInfo from "@/components/auth/UserInfo";
 import React from "react";
 
-export default function Dashboard() {
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Dashboard() {
+  // function to get user session (user login data)
+  const session = await getServerSession(authOptions);
+
+  // if there's no session data, go back to login
+  if (!session) redirect("/auth/login");
+
   return (
     <div>
       <UserInfo />
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,18 @@
 import LoginForm from "@/components/auth/LoginForm";
-import React from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default function Login() {
+export default async function Login() {
+  // try to retrieve the server session using the authOptions object
+  // that contains the authorize(credentials) function that will
+  // return the 'user' ('user' = {user: {firstName, email, password}})
+  const session = await getServerSession(authOptions);
+
+  // if the user session object exists, go to the dashboard
+  // if not go to the login page
+  if (session) redirect("/auth/dashboard");
+
   return (
     <div>
       <LoginForm />
