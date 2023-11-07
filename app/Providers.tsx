42fcd48684210a3cde58e2266235d6d2d3fcff6d
Noah Fajarda <noahfajarda@gmail.com>
2023-11-07 15:40:11 -0800

    created an auth provider wrapper so next-auth functions can be used globally

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,6 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 
+// created an auth provider wrapper so next-auth functions can be used globally
 export const AuthProvider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
