99ccdb28a94f74856f2d48955186442f2cbd6380
Noah Fajarda <noahfajarda@gmail.com>
2023-11-06 19:43:08 -0800

    added toast component from shadcn/ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { Toaster } from '@/components/ui/toaster'
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -25,7 +26,10 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )}
-      >{children}</body>
+      >
+        <Toaster/>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { ToastDemo } from '@/components/toast'
 
 export default function Test() {
   return (
     <div>
       <Button>Click Me</Button>
+      <ToastDemo/>
     </div>
   )
 }
