aaa30d0738ccf7817a3964458b779f54f8d36d3f
Noah Fajarda <noahfajarda@gmail.com>
2023-11-06 20:35:17 -0800

    added combo box component

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,13 +1,37 @@
 import React from "react";
+// button
 import { Button } from "@/components/ui/button";
+// toast
 import { ToastDemo } from "@/components/toast";
+// accordion
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+// alert
 import Alert from "@/components/Alert";
+// popover
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+// command
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+// combo box
+import { ComboboxDemo } from "@/components/combo";
 
 export default function Test() {
   const objectsArray = [
@@ -36,8 +60,10 @@ export default function Test() {
   return (
     <div>
       <Button>Click Me</Button>
+      {/* toast */}
       <ToastDemo />
 
+      {/* accordion */}
       <div className="w-1/3 rounded-lg bg-sky-300 p-3">
         {objectsArray.map((object) => (
           <Accordion type="single" collapsible>
@@ -48,12 +74,40 @@ export default function Test() {
           </Accordion>
         ))}
       </div>
+      {/* alert */}
       <Alert
-        trigger="Open bro"
+        trigger="Open Alert"
         title="this is a title for alert"
         description="This action cannot be undone. This will permanently delete your
             account and remove your data from our servers."
       />
+      {/* popover */}
+      <div className="bg-sky-500 inline p-3 m-5 rounded">
+        <Popover>
+          <PopoverTrigger>Open Popover</PopoverTrigger>
+          <PopoverContent>Place content for the popover here.</PopoverContent>
+        </Popover>
+      </div>
+      {/* command */}
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+      {/* combo box */}
+      <ComboboxDemo />
     </div>
   );
 }
