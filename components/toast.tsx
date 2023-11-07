f0448d8a39925ca5967ef96cb4297b1d66dca9b4
Noah Fajarda <noahfajarda@gmail.com>
2023-11-06 20:20:20 -0800

    edited toast component

--- components/toast.tsx
+++ components/toast.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { useToast } from "@/components/ui/use-toast"
-import { Button } from "./ui/button"
+import { useToast } from "@/components/ui/use-toast";
+import { Button } from "./ui/button";
 import { ToastAction } from "./ui/toast";
 
 // use the toast component from 'use-toast' hook
 export const ToastDemo = () => {
-  const { toast } = useToast()
+  const { toast } = useToast();
 
   return (
     <Button
@@ -16,11 +16,15 @@ export const ToastDemo = () => {
           variant: "destructive",
           title: "Scheduled: Catch up",
           description: "Friday, February 10, 2023 at 5:57 PM",
-          action: <ToastAction altText="Try again">Try again</ToastAction>,
-        })
+          action: (
+            <ToastAction altText="Try again">
+              <a href="/">Back To Homepage</a>
+            </ToastAction>
+          ),
+        });
       }}
     >
       Show Toast
     </Button>
-  )
-}
+  );
+};
