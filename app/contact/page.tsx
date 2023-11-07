9a6c627911d4573cc954720c43ae0af402ba3ef5
Noah Fajarda <noahfajarda@gmail.com>
2023-11-07 12:14:19 -0800

    minor change in contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactData from "@/components/ContactData";
 import ContactForm from "@/components/ContactForm";
 import React from "react";
 
-export default function page() {
+export default function Contact() {
   return (
     <div className="p-4 max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold">Contact Us</h1>
