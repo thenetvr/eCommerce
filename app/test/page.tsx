d9b912e97a6c2cdc25f1582e74300463eebeee6d
Noah Fajarda <noahfajarda@gmail.com>
2023-11-06 20:14:08 -0800

    added an alert with custom props

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,54 +1,59 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import { ToastDemo } from '@/components/toast'
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { ToastDemo } from "@/components/toast";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
+} from "@/components/ui/accordion";
+import Alert from "@/components/Alert";
 
 export default function Test() {
   const objectsArray = [
     {
       title: "Object 1",
-      description: "This is the description for Object 1."
+      description: "This is the description for Object 1.",
     },
     {
       title: "Object 2",
-      description: "This is the description for Object 2."
+      description: "This is the description for Object 2.",
     },
     {
       title: "Object 3",
-      description: "This is the description for Object 3."
+      description: "This is the description for Object 3.",
     },
     {
       title: "Object 4",
-      description: "This is the description for Object 4."
+      description: "This is the description for Object 4.",
     },
     {
       title: "Object 5",
-      description: "This is the description for Object 5."
-    }
+      description: "This is the description for Object 5.",
+    },
   ];
 
   return (
     <div>
       <Button>Click Me</Button>
-      <ToastDemo/>
+      <ToastDemo />
 
-      <div className='w-1/3 rounded-lg bg-sky-300 p-3'>
+      <div className="w-1/3 rounded-lg bg-sky-300 p-3">
         {objectsArray.map((object) => (
           <Accordion type="single" collapsible>
             <AccordionItem value="item-1">
               <AccordionTrigger>{object.title}</AccordionTrigger>
-              <AccordionContent>
-                {object.description}
-              </AccordionContent>
+              <AccordionContent>{object.description}</AccordionContent>
             </AccordionItem>
-            </Accordion>
+          </Accordion>
         ))}
       </div>
+      <Alert
+        trigger="Open bro"
+        title="this is a title for alert"
+        description="This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers."
+      />
     </div>
-  )
+  );
 }
