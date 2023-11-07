3519c8a9c3836e9886a7dde42ce5985eb12d663a
Noah Fajarda <noahfajarda@gmail.com>
2023-11-07 15:37:26 -0800

    created API auth credentials provider that will handle login authentication

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,20 +1,79 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useReducer, useState } from "react";
+
+// Interface for our state
+interface FormState {
+  email: string;
+  password: string;
+}
 
 export default function LoginForm() {
+  const router = useRouter();
+
+  const [state, dispatch] = useReducer(
+    (state: FormState, action: Partial<FormState>) => ({
+      ...state,
+      ...action,
+    }),
+    {
+      email: "",
+      password: "",
+    }
+  );
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { email, password } = state;
+
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
+
+      router.replace("/auth/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-        <h1 className="text-xl font-bold my-4">Login</h1>
-        <form className="flex flex-col gap-3">
-          <input type="text" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+        <h1 className="text-xl font-bold my-4">Log In</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+          <input
+            type="text"
+            placeholder="Email"
+            value={state.email}
+            onChange={(e) => dispatch({ email: e.target.value })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={state.password}
+            onChange={(e) => dispatch({ password: e.target.value })}
+          />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
-            Login
+            Log In
           </button>
-          <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-            Error message
-          </div>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
           <Link className="text-sm mt-3 text-right" href={"/auth/signup"}>
             Don't have an account? <span className="underline">Sign Up</span>
           </Link>
