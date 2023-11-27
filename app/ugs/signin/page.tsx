6b17071066a5ddb7c4568a43f270561d06985a86
mikexie360 <mxie360@gmail.com>
2023-11-27 12:08:01 -0600

    publish sign in sign up

---
+++ app/ugs/signin/page.tsx
@@ -0,0 +1,21 @@
+import LoginForm from "@/components/auth/LoginForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Login() {
+  // try to retrieve the server session using the authOptions object
+  // that contains the authorize(credentials) function that will
+  // return the 'user' ('user' = {user: {firstName, email, password}})
+//   const session = await getServerSession(authOptions);
+
+  // if the user session object exists, go to the dashboard
+  // if not go to the login page
+//   if (session) redirect("/auth/dashboard");
+
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
---
+++ app/ugs/signup/page.tsx
@@ -0,0 +1,21 @@
+import SignupForm from "@/components/auth/SignupForm";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+export default async function Signup() {
+  // try to retrieve the server session using the authOptions object
+  // that contains the authorize(credentials) function that will
+  // return the 'user' ('user' = {user: {firstName, email, password}})
+//   const session = await getServerSession(authOptions);
+
+  // if the user session object exists, go to the dashboard
+  // if not go to the signup page
+//   if (session) redirect("/auth/dashboard");
+
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  );
+}
