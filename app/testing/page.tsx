9907973123a2cbd192286767a9b724abea682459
Noah Fajarda <noahfajarda@gmail.com>
2023-12-01 08:38:15 -0800

    added hero for home

---
+++ app/testing/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/testing/hero";
+import LearnMore from "@/components/testing/learnMore";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <Hero />
+      <LearnMore />
+    </div>
+  );
+}
---
+++ components/testing/Hero.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="h-96 flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center text-center">
+        <h1 className="text-4xl text-center sm:w-96 w-full font-bold p-2">
+          The Net VR E-Commerce Platform
+        </h1>
+        <p className="p-2 sm:w-96 w-full">
+          Build a blockchain using the best-in-class open source libraries and
+          services
+        </p>
+      </div>
+    </div>
+  );
+}
