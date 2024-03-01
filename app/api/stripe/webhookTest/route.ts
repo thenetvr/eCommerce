d23af16d7c365f3bf53259d426dd087a60395e2f
mikexie360 <mxie360@gmail.com>
2024-02-29 22:30:05 -0500

    Update route.ts

--- app/api/stripe/webhookTest/route.ts
+++ app/api/stripe/webhookTest/route.ts
@@ -8,11 +8,11 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 const endpointSecret = process.env.WEBHOOK_SECRET as string;
 
 // Make sure to add this, otherwise you will get a stream.not.readable error
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
 export default async function handler(
   req: NextApiRequest,
