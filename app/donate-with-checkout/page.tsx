71e593ee6b2897bd10b05948b1c301e914c39aa1
mikexie360 <mxie360@gmail.com>
2024-03-07 20:11:23 -0500

    initial commit

---
+++ app/donate-with-checkout/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+import CheckoutForm from "@/components/CheckoutForm";
+
+export const metadata: Metadata = {
+  title: "Donate with hosted Checkout | Next.js + TypeScript Example",
+};
+
+export default function DonatePage(): JSX.Element {
+  return (
+    <div className="page-container">
+      <h1>Buy LuxRays with hosted Checkout</h1>
+      <p>Buy LuxRays!</p>
+      <CheckoutForm uiMode="hosted" />
+    </div>
+  );
+}
